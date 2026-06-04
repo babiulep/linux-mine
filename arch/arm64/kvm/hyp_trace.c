77410497501d0ed0511f40ad04d391f1510b644e
Willy Frissen <babiulep@gmail.com>
2026-06-04 14:00:03 +0200

    tip-0f525d827b3fdd4bd18c2e23ffea84e523e07ab2

--- arch/arm64/kvm/hyp_trace.c
+++ arch/arm64/kvm/hyp_trace.c
@@ -118,7 +118,7 @@ static void hyp_trace_clock_enable(struct hyp_trace_clock *hyp_clock, bool enabl
 		hyp_clock->running = false;
 	}
 
-	ktime_get_snapshot_id(&snap, CLOCK_BOOTTIME);
+	ktime_get_snapshot_id(CLOCK_BOOTTIME, &snap);
 
 	hyp_clock->boot = ktime_to_ns(snap.systime);
 	hyp_clock->cycles = snap.cycles;
@@ -189,7 +189,7 @@ static void hyp_trace_buffer_unshare_hyp(struct hyp_trace_buffer *trace_buffer, 
 		if (cpu > last_cpu)
 			break;
 
-		__unshare_page(rb_desc->meta_va);
+		__share_page(rb_desc->meta_va);
 		for (p = 0; p < rb_desc->nr_page_va; p++)
 			__unshare_page(rb_desc->page_va[p]);
 	}
@@ -212,15 +212,14 @@ static int hyp_trace_buffer_share_hyp(struct hyp_trace_buffer *trace_buffer)
 		}
 
 		if (ret) {
-			while (--p >= 0)
+			for (p--; p >= 0; p--)
 				__unshare_page(rb_desc->page_va[p]);
-			__unshare_page(rb_desc->meta_va);
 			break;
 		}
 	}
 
 	if (ret)
-		hyp_trace_buffer_unshare_hyp(trace_buffer, --cpu);
+		hyp_trace_buffer_unshare_hyp(trace_buffer, cpu--);
 
 	return ret;
 }
@@ -249,7 +248,6 @@ static struct trace_buffer_desc *hyp_trace_load(unsigned long size, void *priv)
 		goto err_free_desc;
 
 	trace_buffer->desc = desc;
-	trace_buffer->desc_size = desc_size;
 
 	ret = hyp_trace_buffer_alloc_bpages_backing(trace_buffer, size);
 	if (ret)
@@ -299,7 +297,6 @@ static void hyp_trace_unload(struct trace_buffer_desc *desc, void *priv)
 	hyp_trace_buffer_free_bpages_backing(trace_buffer);
 	free_pages_exact(trace_buffer->desc, trace_buffer->desc_size);
 	trace_buffer->desc = NULL;
-	trace_buffer->desc_size = 0;
 }
 
 static int hyp_trace_enable_tracing(bool enable, void *priv)
