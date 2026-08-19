bdc6f308722a6bc2ae0c39a0202db73599bade93
Willy Frissen <babiulep@gmail.com>
2026-08-19 09:19:30 +0200

    tip-6d3a145276d7

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1200,6 +1200,7 @@ struct task_struct {
 	unsigned long			last_switch_time;
 #endif
 	/* Filesystem information: */
+	struct fs_struct		*real_fs;
 	struct fs_struct		*fs;
 
 	/* Open file information: */
