d38a6f414eec1710efa440aeb645b1a07518f830
Willy Frissen <babiulep@gmail.com>
2026-09-16 20:54:31 +0200

    KVM: Serialize vCPU creation and revert vcpu_ids tracking
    https://lore.kernel.org/all/20260914181223.289061-1-seanjc@google.com/
    
    Serialize vCPU creation by holding kvm->lock for the entirety of
    kvm_vm_ioctl_create_vcpu(), and then revert the now-redundant tracking adding
    by commit 97d65b544f48 ("KVM: Check for duplicate vcpu_id as early as
    possible").  I botched the math when justifying the vcpu_ids tracking; it's not
    an extra 256 bytes, it's an extra 2048 bytes.  Roughly doubling the size of
    "struct kvm" tripped x86's KVM_SANITY_CHECK_VM_STRUCT_SIZE, and obviously isn't
    something we want to do in general.
    
    The TL;DR of why it's a-ok to serialize vCPU creation is that no VMM actually
    does parallel vCPU creation.  As with so many things, KVM's current behavior is
    the result of decades-old cruft, not intentional, deliberate design.
    
    Patch 1 is a tangentially related bug fix; I included it here because holding
    kvm->lock for all of vCPU creation allows WARNing if KVM attempts to lock all
    vCPUs if vCPU creation is in-progress (the caller is must hold kvm->lock).
    
    Sean Christopherson (5):
      KVM: Reject attempts to lock all vCPUs if vCPU creation is in-progress
      KVM: Protect all of kvm_vm_ioctl_create_vcpu() with kvm->lock
      KVM: Move check for existing vCPU ID to the top of vCPU creation
      Revert "KVM: Check for duplicate vcpu_id as early as possible"
      KVM: WARN if vCPU creation is in-progress when locking all vCPUs

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1373,6 +1373,9 @@ int kvm_trylock_all_vcpus(struct kvm *kvm)
 
 	lockdep_assert_held(&kvm->lock);
 
+	if (WARN_ON_ONCE(kvm_is_vcpu_creation_in_progress(kvm)))
+		return -EBUSY;
+
 	kvm_for_each_vcpu(i, vcpu, kvm)
 		if (!mutex_trylock_nest_lock(&vcpu->mutex, &kvm->lock))
 			goto out_unlock;
@@ -1395,6 +1398,9 @@ int kvm_lock_all_vcpus(struct kvm *kvm)
 	int r;
 
 	lockdep_assert_held(&kvm->lock);
+
+	if (WARN_ON_ONCE(kvm_is_vcpu_creation_in_progress(kvm)))
+		return -EBUSY;
 
 	kvm_for_each_vcpu(i, vcpu, kvm) {
 		r = mutex_lock_killable_nest_lock(&vcpu->mutex, &kvm->lock);
@@ -4169,6 +4175,8 @@ static int kvm_vm_ioctl_create_vcpu(struct kvm *kvm, unsigned long id)
 	struct kvm_vcpu *vcpu;
 	struct page *page;
 
+	guard(mutex)(&kvm->lock);
+
 	/*
 	 * KVM tracks vCPU IDs as 'int', be kind to userspace and reject
 	 * too-large values instead of silently truncating.
@@ -4181,26 +4189,17 @@ static int kvm_vm_ioctl_create_vcpu(struct kvm *kvm, unsigned long id)
 	if (id >= KVM_MAX_VCPU_IDS)
 		return -EINVAL;
 
-	mutex_lock(&kvm->lock);
-	if (kvm->created_vcpus >= kvm->max_vcpus) {
-		mutex_unlock(&kvm->lock);
+	if (kvm->created_vcpus >= kvm->max_vcpus)
 		return -EINVAL;
-	}
 
-	if (test_bit(id, kvm->vcpu_ids)) {
-		mutex_unlock(&kvm->lock);
+	if (kvm_get_vcpu_by_id(kvm, id))
 		return -EEXIST;
-	}
 
 	r = kvm_arch_vcpu_precreate(kvm, id);
-	if (r) {
-		mutex_unlock(&kvm->lock);
+	if (r)
 		return r;
-	}
 
 	kvm->created_vcpus++;
-	__set_bit(id, kvm->vcpu_ids);
-	mutex_unlock(&kvm->lock);
 
 	vcpu = kmem_cache_zalloc(kvm_vcpu_cache, GFP_KERNEL_ACCOUNT);
 	if (!vcpu) {
@@ -4229,13 +4228,6 @@ static int kvm_vm_ioctl_create_vcpu(struct kvm *kvm, unsigned long id)
 					 id, kvm->dirty_ring_size);
 		if (r)
 			goto arch_vcpu_destroy;
-	}
-
-	mutex_lock(&kvm->lock);
-
-	if (WARN_ON_ONCE(kvm_get_vcpu_by_id(kvm, id))) {
-		r = -EEXIST;
-		goto unlock_vcpu_destroy;
 	}
 
 	/*
@@ -4271,7 +4263,6 @@ static int kvm_vm_ioctl_create_vcpu(struct kvm *kvm, unsigned long id)
 	atomic_inc(&kvm->online_vcpus);
 	mutex_unlock(&vcpu->mutex);
 
-	mutex_unlock(&kvm->lock);
 	kvm_arch_vcpu_postcreate(vcpu);
 	kvm_create_vcpu_debugfs(vcpu);
 	return r;
@@ -4282,7 +4273,6 @@ kvm_put_xa_erase:
 	xa_erase(&kvm->vcpu_array, vcpu->vcpu_idx);
 unlock_vcpu_destroy:
 	vcpu->vcpu_idx = -1;
-	mutex_unlock(&kvm->lock);
 	kvm_dirty_ring_free(&vcpu->dirty_ring);
 arch_vcpu_destroy:
 	kvm_arch_vcpu_destroy(vcpu);
@@ -4291,10 +4281,7 @@ vcpu_free_run_page:
 vcpu_free:
 	kmem_cache_free(kvm_vcpu_cache, vcpu);
 vcpu_decrement:
-	mutex_lock(&kvm->lock);
 	kvm->created_vcpus--;
-	__clear_bit(id, kvm->vcpu_ids);
-	mutex_unlock(&kvm->lock);
 	return r;
 }
 
